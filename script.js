b9b8a13eb820967a0294774efe3ecf755b4025ee
charlx99 <charlotteuxd@gmail.com>
2024-02-13 19:12:06 +0800

    API Integration
    Integrated moment.js API to the world clock.

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-// alert("Hello");
+function updateTime() {
+  //los Angeles Time
+  let losAngelesElement = document.querySelector("#los-angeles");
+  let losAngelesDateElement = losAngelesElement.querySelector(".date");
+  let losAngelesTimeElement = losAngelesElement.querySelector(".time");
+  let losAngelesTime = moment().tz("America/Los_Angeles");
+
+  losAngelesDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  losAngelesTimeElement.innerHTML = losAngelesTime.format(
+    "hh:mm:ss [<small>]A[</small>]"
+  );
+
+  // Paris Time
+  let ParisElement = document.querySelector("#paris");
+  let ParisDateElement = ParisElement.querySelector(".date");
+  let ParisTimeElement = ParisElement.querySelector(".time");
+  let ParisTime = moment().tz("Europe/Paris");
+
+  ParisDateElement.innerHTML = moment().format("MMMM Do YYYY");
+  ParisTimeElement.innerHTML = ParisTime.format(
+    "hh:mm:ss [<small>]A[</small>]"
+  );
+}
+
+updateTime();
+setInterval(updateTime, 1000);
