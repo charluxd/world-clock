766bb38d5a42c5d19fc9e4e08f3b899073b8406f
charlx99 <charlotteuxd@gmail.com>
2024-02-15 09:33:19 +0800

    Added Current Location
    Added current location to the html & javascript.

--- script.js
+++ script.js
@@ -28,6 +28,9 @@ function updateTime() {
 
 function updateCity(event) {
   let cityTimeZone = event.target.value;
+  if (cityTimeZone === "current") {
+    cityTimeZone = moment.tz.guess();
+  }
   let cityName = cityTimeZone.replace("_", " ").split("/")[1];
   let cityTime = moment().tz(cityTimeZone);
   let citiesElement = document.querySelector("#cities");
@@ -48,5 +51,4 @@ updateTime();
 setInterval(updateTime, 1000);
 
 let citiesSelectElement = document.querySelector("#select-city");
-
 citiesSelectElement.addEventListener("change", updateCity);
